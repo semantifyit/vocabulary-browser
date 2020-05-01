bea7e5176922f316bef7d20afb85e3a819a391a3
Shakeel Ahmed <xhakil26@gmail.com>
2020-05-01 16:09:51 +0200

    new tab target for semantify vocab

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,6 +1,6 @@
-$.getJSON("vocabs.JSON", function (data) {
+$.getJSON("vocabs.JSON", function(data) {
     let dataset = data[0]['schema:dataset'];
-    dataset.forEach(function (set, index) {
+    dataset.forEach(function(set, index) {
         let nameofvocab = [set][0]['schema:name'];
         let vocabUrl = [set][0]['schema:url'];
         let authorName = [set][0]['schema:author']['schema:name'];
@@ -9,17 +9,16 @@ $.getJSON("vocabs.JSON", function (data) {
         let vocabId = splitURL[4];
         $('#external-vocab').append(`<tr id="vocab-${index}"></tr>`);
         $(`#vocab-${index}`).append(`<td><a class="nameofvocab" href="./${vocabId}">${nameofvocab}</a></td>`);
-        $(`#vocab-${index}`).append(`<td><a href="${vocabUrl}">${vocabUrl}</a></td>`);
+        $(`#vocab-${index}`).append(`<td><a href="${vocabUrl}" target="_blank">${vocabUrl}</a></td>`);
         if (authorName) {
             $(`#vocab-${index}`).append(`<td>${authorName}</td>`);
-        }
-        else {
+        } else {
             $(`#vocab-${index}`).append(`<td>${authorEmail}</td>`);
         }
     });
 });
 
-$(document).delegate('.nameofvocab', 'click', function (e) {
+$(document).delegate('.nameofvocab', 'click', function(e) {
     e.preventDefault();
     let vocabularyName = $(this).text();
     localStorage.setItem("vocab", vocabularyName);
