12ed072bb59c0e5183b2c4ed2e9ad8f2d3023e0f
Shakeel Ahmed <xhakil26@gmail.com>
2020-05-03 22:51:09 +0200

    app.use for bootstrap, jquery and schema-adapter

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
 const app = express();
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 app.use('/lib/schema-org-adapter.min.js', express.static(__dirname + '/node_modules/schema-org-adapter/dist/schema-org-adapter.min.js')); //sdo library
 app.use(express.static('public'));
+
+const node_modules_path = require.resolve('express').split('express')[0].slice(0, -1);
+app.use('/libs/jquery', express.static(node_modules_path + '/jquery/dist/'));
+app.use('/libs/bootstrap', express.static(node_modules_path + '/bootstrap/dist/'));
+app.use('/libs/schema-org-adapter', express.static(node_modules_path + '/schema-org-adapter/dist/'));
+
 
 app.get('/', (req, res) => {
     //res.sendFile(__dirname + '/public/index.html');
@@ -26,7 +32,7 @@ app.get('/:vocabId', (req, res) => {
 app.get('/*', (req, res) => {
 
     res.sendFile(__dirname + '/public/vocabTerm.html');
-//    res.sendFile(__dirname + '/public/welcome.html');
+    //    res.sendFile(__dirname + '/public/welcome.html');
 
 });
 
