26ded3a2981756a413920fc882cea2e4057711cd
Shakeel Ahmed <xhakil26@gmail.com>
2020-05-15 15:46:29 +0200

    Added description/summar on index page

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -4,11 +4,34 @@ $(document).ready(async() => {
     let dataset = json[0]['schema:dataset'];
     let template = await $.get('/templateWelcome.html');
 
+    let vocabularyDesc = '';
+
     dataset.forEach((set, index) => {
         let nameofvocab = [set][0]['schema:name'];
         let vocabUrl = [set][0]['schema:url'];
         let authorName = [set][0]['schema:author']['schema:name'];
         let authorEmail = [set][0]['schema:author']['schema:email'];
+
+        let additionalProperty = [set][0]['schema:additionalProperty'];
+        let classesInJson = '';
+        let propertiesInJson = '';
+        if (additionalProperty) {
+            for (let i = 0; i < additionalProperty.length; i++) {
+                if (additionalProperty[i]["schema:name"]) {
+                    if (additionalProperty[i]["schema:name"] === "Classes") {
+                        classesInJson = `<br> <b>Summary:</b> ${additionalProperty[i]["schema:value"]} Classes,`
+                    } else if (additionalProperty[i]["schema:name"] === "Properties") {
+                        propertiesInJson = `${additionalProperty[i]["schema:value"]} Properties`
+                    }
+                }
+            }
+        }
+        if ([set][0]['schema:description']) {
+            vocabularyDesc = [set][0]['schema:description']
+        } else {
+            vocabularyDesc = '';
+        }
+
         let splitURL = vocabUrl.split('/');
         let vocabId = splitURL[4];
 
@@ -18,6 +41,9 @@ $(document).ready(async() => {
             .replace(/{{nameofvocab}}/g, nameofvocab)
             .replace(/{{vocabUrl}}/g, vocabUrl)
             .replace(/{{author}}/g, authorName || authorEmail)
+            .replace(/{{vocabularyDesc}}/g, vocabularyDesc)
+            .replace(/{{classesInJson}}/g, classesInJson)
+            .replace(/{{propertiesInJson}}/g, propertiesInJson)
         );
     });
 });
